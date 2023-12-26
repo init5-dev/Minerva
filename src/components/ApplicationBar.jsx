d08e280ecf7b3adf0480e2a941036dbbddb026ce
Nelson Ochagavía <nelson.ochagavia@gmail.com>
2023-12-26 15:53:57 -0500

    (fix) search box: temporaly disabled

--- src/components/ApplicationBar.jsx
+++ src/components/ApplicationBar.jsx
@@ -1,20 +1,21 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+//import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
+//import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+//import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import { Settings } from "@mui/icons-material";
 
+/*
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -54,6 +55,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
+*/
 
 export default function ApplicationBar({ drawerState }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
